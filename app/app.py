7c2b77bc77d483b0fa79e7634582c0533beb565e
Bell Amancio de Melo Costa <bell.costa@buriti.br>
2024-07-01 15:06:43 -0300

    Setting utils

--- app/app.py
+++ app/app.py
@@ -1,4 +1,7 @@
 import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from flask import request, jsonify, Flask
 from flask_cors import CORS
 from PIL import Image, ImageDraw, ImageFont
